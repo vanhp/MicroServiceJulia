c3d0206d0f389ca9e303c1039ac3f8acfad1c6f8
vanh <vanhphom@gmail.com>
2020-07-29 22:11:36 -0700

    Files generated by PkgTemplates

---
+++ src/MicroServiceJulia.jl
@@ -0,0 +1,5 @@
+module MicroServiceJulia
+
+# Write your package code here.
+
+end
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using MicroServiceJulia
+using Test
+
+@testset "MicroServiceJulia.jl" begin
+    # Write your tests here.
+end
